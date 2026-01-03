54e6f138aad415a7f4be13c66cd2d85786016cd0
aklanulh <aklanulh@gmail.com>
2026-01-03 15:45:48 +0700

    Fix favicon paths to include public directory for Hostinger

--- resources/views/report-history.blade.php
+++ resources/views/report-history.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Riwayat Laporan - CatatanKerja</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="icon" type="image/png" href="{{ url('/favicons/briefcase.png') }}">
+    <link rel="icon" type="image/png" href="{{ url('/public/favicons/briefcase.png') }}">
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
