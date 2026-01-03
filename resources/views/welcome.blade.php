8e9213bfe47dfd431276db2fdf8bee62f8dd206b
aklanulh <aklanulh@gmail.com>
2026-01-03 15:09:21 +0700

    Fix favicon paths for subdomain compatibility using url() helper

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
-        <link rel="icon" type="image/svg+xml" href="/ck/favicons/brieftcase.svg">
+        <link rel="icon" type="image/svg+xml" href="{{ url('/favicons/brieftcase.svg') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
