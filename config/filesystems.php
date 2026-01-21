bb6ebd1216b586c9c73dd8f18637e39378efaf5c
aklanulh <aklanulh@gmail.com>
2026-01-21 09:44:12 +0700

    Fix public disk URL - change from /public/storage to /storage

--- config/filesystems.php
+++ config/filesystems.php
@@ -41,7 +41,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => public_path('storage'),
-            'url' => env('APP_URL') . '/public/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
